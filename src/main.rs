67641f80229f6306dee834c7866ff013a80257e7
Stan-breaks <stanleymwendwa03@gmail.com>
2025-06-11 14:17:00 +0300

    simple counter demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+
+#[derive(Debug, Default)]
+struct App {
+    counter: u8,
+    exit: bool,
+}
+
+impl App {
+    fn new() -> Self {
+        Self::default()
+    }
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_event()?;
+        }
+        Ok(())
+    }
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+    fn handle_event(&mut self) -> io::Result<()> {
+        todo!()
+    }
+}
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from("Counter app Tutorial".bold());
+        let instructions = Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            ",<q>".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title)
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::new().run(&mut terminal);
+    ratatui::restore();
+    app_result
 }
