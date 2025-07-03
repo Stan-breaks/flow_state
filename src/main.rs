651da8da719ebdb456756b5d56cba939e4e91391
Stan-breaks <stanleymwendwa03@gmail.com>
2025-07-03 22:27:12 +0300

    deleting habits

--- src/main.rs
+++ src/main.rs
@@ -115,6 +115,38 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> color_eyre:
                                 app.habits[index].toggle_complete();
                             }
                         }
+                        KeyCode::Char('d') => {
+                            if !app.counter.switch && app.counter.build_counter > 0 {
+                                let build_habit = app
+                                    .habits
+                                    .iter()
+                                    .filter(|habit| habit.habit_type == HabitType::Build)
+                                    .collect::<Vec<&Habit>>()[app.counter.build_counter - 1];
+                                let mut index = 0;
+                                for i in 0..app.habits.len() {
+                                    if &app.habits[i] == build_habit {
+                                        index = i;
+                                        break;
+                                    }
+                                }
+                                app.habits.remove(index);
+                            }
+                            if app.counter.switch && app.counter.avoid_counter > 0 {
+                                let avoid_habit = app
+                                    .habits
+                                    .iter()
+                                    .filter(|habit| habit.habit_type == HabitType::Avoid)
+                                    .collect::<Vec<&Habit>>()[app.counter.avoid_counter - 1];
+                                let mut index = 0;
+                                for i in 0..app.habits.len() {
+                                    if &app.habits[i] == avoid_habit {
+                                        index = i;
+                                        break;
+                                    }
+                                }
+                                app.habits.remove(index);
+                            }
+                        }
                         _ => {}
                     },
                     ScreenMode::Adding => match key.code {
