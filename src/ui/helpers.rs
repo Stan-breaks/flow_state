315cd97189998e43843f12878ad3e278c09e5152
Stan-breaks <stanleymwendwa03@gmail.com>
2026-02-20 20:50:08 +0300

    feat: ui helper

---
+++ src/ui/helpers.rs
@@ -0,0 +1,24 @@
+use ratatui::{
+    layout::{Constraint, Direction, Layout, Rect},
+};
+
+/// Create a centered rectangle within the given area using percentage-based sizing.
+pub fn centered_rect(area: Rect, width_pct: u16, height_pct: u16) -> Rect {
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - height_pct) / 2),
+            Constraint::Percentage(height_pct),
+            Constraint::Percentage((100 - height_pct) / 2),
+        ])
+        .split(area);
+
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - width_pct) / 2),
+            Constraint::Percentage(width_pct),
+            Constraint::Percentage((100 - width_pct) / 2),
+        ])
+        .split(popup_layout[1])[1]
+}
